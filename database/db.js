4f87c69bd0b464d43be28216f89a5294dbc93000
saitTalhaNisanci <s.talhanisanci@gmail.com>
2018-02-17 08:57:52 +0300

    use promise to connect to database

--- database/db.js
+++ database/db.js
@@ -1,15 +1,26 @@
-var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 var connectionUrl = "mongodb://heroku_sn4pp7mh:b7j0q7h9mkacnh7ce2c6icmpnd@ds239648.mlab.com:39648/heroku_sn4pp7mh";
 var defaultLogger = require('../logging/defaultLogger');
 var log = new defaultLogger();
-/*
-Connect to the database with the given url.
-If database does not exit it will be created.
-*/
-MongoClient.connect(connectionUrl, function(err, db) {
-  if (err) {
-    log.err(err);  
-    throw err;
-  }
-  log.info("Connected to database!");
+
+// a function to connect to the given databaseUrl asyncly.
+function connectToDatabase(databaseUrl){
+  return new Promise(function(resolve,reject){
+      mongoose.connect(databaseUrl,function(err,database){
+        if(err){
+          log.err(err);
+          reject(err);
+        }else {
+          log.info("Connected to database!");
+          resolve(database);
+        }
+      });
+  });
+};
+connectToDatabase(connectionUrl)
+.then(function(database){    
+  
+})
+.catch(function(err){
+  console.log(err);
 });
