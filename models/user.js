d9229da3499e6438ad2279136e31379bf9a9f9ac
saitTalhaNisanci <s.talhanisanci@gmail.com>
2018-02-17 23:51:47 +0300

    add updateLocation endpoint for user

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose'),
 
 /*
     username: username is a unique name that user chooses.
+    location : last known location of the user.
     rate: rate has two components
         score: totalScore of the user
         amount: how many times the user was rated.
@@ -12,6 +13,7 @@ var mongoose = require('mongoose'),
 var userSchema  = new Schema (
     {
         username: {type:String,unique:true},
+        location: { type: {type:String}, coordinates: [Number]},
         rate:{
             score:{type:Number},
             amount:{type:Number}
--- routes/users.js
+++ routes/users.js
@@ -42,3 +42,66 @@ router.post('/create', (req,res) => {
   });
 
 module.exports = router;
+
+/*
+ * updateLocation is an async function to update a user
+ * with the given update data.
+ * condition data:
+ * { username: req.body.username }
+ * update data:
+ * {
+ *  location : {
+ *    type: req.body.type,
+ *    coordinates: [
+ *      req.body.latitude, req.body.longitude
+ *    ]
+ *  }
+ * }
+ */  
+var updateLocation = (conditions,update,options) => {
+  return new Promise((resolve,reject) => {
+    userModel.findOneAndUpdate(conditions,update, options,(err,doc) => {
+      if (!err && doc){
+        log.info("User location is successully updated");
+        resolve(doc);
+      }else {
+        log.err(err);
+        reject(err);
+      }
+    })
+  })
+}
+
+/* PUT update an existing user.
+ * 'username' has to be provided as validation.
+ *  a nonexisting username will result in 400 response.
+ *  a valid update data has the following form:
+ *  {
+ *	 "username":"heisenberg1",
+ *   "longitude": 10,
+ *	 "latitude":20,
+ *	 "type":"Point"
+ *   }
+ */
+router.put('/updateLocation',(req,res) => {
+  var conditions = {
+    username: req.body.username,
+  }
+  var update = {
+    location : {
+      type: req.body.type,
+      coordinates: [
+        req.body.latitude, req.body.longitude
+      ]
+    }
+  }
+  var options = {new: true, upsert: true};
+  updateLocation(conditions,update)
+    .then((doc) => {
+      res.status(200).send(doc);
+    })
+    .catch((err) => {
+      log.err(err);
+      res.status(400).send(err);
+    })
+})
